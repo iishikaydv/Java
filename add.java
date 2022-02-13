6982882b00d5ae154084f67857f8eddc04494c80
iishikaydv <yishika308@gmail.com>
2022-02-13 23:31:25 +0530

    trying java

---
+++ add.java
@@ -0,0 +1,17 @@
+/*import java.util.*;
+public class add {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //addition
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+        System.out.println(c);
+
+    }
+}*/
+public class add{
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+    } 
+}
