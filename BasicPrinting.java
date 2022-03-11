d9f2bf09a65c9f6a07c745780288ed5b5efa546f
iishikaydv <yishika308@gmail.com>
2022-03-11 22:26:49 +0530

    19th January Done!!!

---
+++ BasicPrinting.java
@@ -0,0 +1,47 @@
+public class BasicPrinting {
+    public static void main(String[] args) {
+		// Print z
+		/*System.out.println("*****");
+		System.out.println("   *");
+		System.out.println("  *");
+		System.out.println(" *");
+		System.out.println("*****");*/
+		
+		// Print T
+		/*System.out.println("*******");
+		System.out.println("   *");
+		System.out.println("   *");
+		System.out.println("   *");
+		System.out.println("   *");
+		System.out.println("   *");
+		System.out.println("   *");*/
+		
+		// Print x
+		/*System.out.println("*   *");
+		System.out.println(" * *");
+		System.out.println("  *");
+		System.out.println(" * *");
+		System.out.println("*   *");*/
+		
+		// Even or Odd
+		/*int a = 54;
+		if (a % 2 == 0) {
+			System.out.println("Number is Even!!!");
+		} else {
+			System.out.println("Number is Odd!!!");
+		}*/
+		
+		// Largest of them All
+		int a = 23;
+		int b = 45;
+		int c = 12;
+		if (a > b && a > c) {
+			System.out.println("a is the Largest!!!");
+		} else if (b > c){
+			System.out.println("b is the Largest!!!");
+		} else {
+			System.out.println("c is the Largest!!!");
+		}
+
+	}
+}
